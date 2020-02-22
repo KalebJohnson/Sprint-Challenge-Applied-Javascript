faa7df8e52278182eb01760f3c246c1fcccd74a2
unknown <KalebJohnson45@yahoo.com>
2020-02-21 19:37:41 -0600

    save

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function Card(imgUrl) {
+    const 
+      newCard = document.createElement("div"),
+      headline = document.createElement("div"),
+      author = document.createElement("div"),
+      imgContainer = document.createElement("div"),
+      newImage = document.createElement("img"),
+      authorSpan = document.createElement("span");
+  
+
+    newImage.src = imgUrl;
+    newCard.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imgContainer.classList.add("img-container");
+    authorSpan.textContent = `By %{authorName}`;
+
+    newCard.appendChild(headline);
+    newCard.appendChild(author);
+    newCard.appendChild(imgContainer);
+    newCard.appendChild(newImage);
+    newCard.appendChild(authorSpan);
+  
+    // add this functionality later
+    newCard.addEventListener("click", () => {
+      newCard.classList.toggle("selected");
+    });
+  
+    return newCard;
+  }
+  
+const cardsContainer = document.querySelector('.cards-container') 
+
+
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response);
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+ //   response.data.articles.javascript.forEach(item => {
+ //     const newDog = Card(item);
+  //    entryPoint.appendChild(newDog);
+ //   });
+ // })
+
+ Card()
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,40 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+/*
+<div class="tabs">
+<div class="topics">
+  <span class="title">TRENDING TOPICS:</span>
+</div>
+</div>
+*/
+function topicCard(item) {
+    const 
+      tabs = document.createElement("div");
+      tabs.classList.add('topics');
+      tabs.textContent = item;
+  
+    const 
+      titleSpan = document.createElement("span")
+      titleSpan.classList.add('title')
+      titleSpan.textContent = "TRENDING TOPICS:";
+      
+      tabs.appendChild(titleSpan)
+  
+    return tabs;
+  }
+
+
+
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+    response.data.topics.forEach(item => {
+        const tabs = topicCard(item);
+        entryPoint.appendChild(tabs);
+      });
+  })
+  const entryPoint = document.querySelector(".topics");
+  topicCard()
